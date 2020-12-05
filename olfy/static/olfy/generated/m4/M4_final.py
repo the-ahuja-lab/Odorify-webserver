8503051f6861c6239414e1bd0354562ccd04f74d
Ria Gupta <ria18405@iiitd.ac.in>
2020-12-05 13:58:57 +0530

    Update M4_final.py

--- olfy/static/olfy/generated/m4/M4_final.py
+++ olfy/static/olfy/generated/m4/M4_final.py
@@ -173,7 +173,7 @@ def user_predict(model, x_input_smile, x_input_seq,count,path):
             
     print(cropped_smile_relevance)
     ax=cropped_smile_relevance.plot( y=["positive", "negative"], color=['green', 'red'], kind="bar", figsize=(25,15))
-    ax.legend(['Contribution to Binding', 'Contribution to non binding'])
+    ax.legend(['Contribution to Binding', 'Contribution to non binding'],prop={'size': 16})
     ax.set_xticklabels(cropped_smile_relevance['smile_char'],fontsize=15,rotation=0)
     ax.set_xlabel("Smiles",fontsize=15)
     ax.set_ylabel("Relevance",fontsize=15,rotation=0)
