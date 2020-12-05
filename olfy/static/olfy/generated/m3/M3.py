f2de176645e2ff762b7ec7e6b735f414dd531c5e
Ria Gupta <ria18405@iiitd.ac.in>
2020-12-05 14:00:22 +0530

    Update M3.py

--- olfy/static/olfy/generated/m3/M3.py
+++ olfy/static/olfy/generated/m3/M3.py
@@ -192,7 +192,7 @@ def combined_user_predict(model, x_input_seq, x_input_smile, filename,path):
             
     print(cropped_smile_relevance)
     ax=cropped_smile_relevance.plot( y=["positive", "negative"], color=['green', 'red'], kind="bar", figsize=(25,15))
-    ax.legend(['Contribution to Binding', 'Contribution to non binding'])
+    ax.legend(['Contribution to Binding', 'Contribution to non binding'],prop={'size': 16})
     ax.set_xticklabels(cropped_smile_relevance['smile_char'],fontsize=15,rotation=0)
     ax.set_xlabel("Smiles", fontsize=15)
     ax.set_ylabel("Relevance", fontsize=15)
